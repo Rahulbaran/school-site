9a715199f19ac9aa30b4bf5e82c9405256e8049d
Rahulbaran <rahulkumar109422@gmail.com>
2023-01-15 18:09:12 +0530

    ci: create CardsData file inside Home directory
    - CardsData file contains CDN links of images of cards

---
+++ src/pages/Home/data/CardsData.jsx
@@ -0,0 +1,10 @@
+export default {
+  class_img:
+    "https://ik.imagekit.io/rahulbaran/school-site/homepage_icons/online-classes.png",
+  fee_img:
+    "https://ik.imagekit.io/rahulbaran/school-site/homepage_icons/online-fees.png",
+  report_img:
+    "https://ik.imagekit.io/rahulbaran/school-site/homepage_icons/online-report.png",
+  exam_img:
+    "https://ik.imagekit.io/rahulbaran/school-site/homepage_icons/online-exam.png"
+};
