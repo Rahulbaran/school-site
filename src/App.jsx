70aaa179e2680daf85e4e306875f51ebb4718075
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-06 08:01:59 +0530

    ci: import pages in App
    - imported all the remaining pages (About, Photos, Videos, Rules, Academics, Contact) and GalleryLayout
    - Used them in createBrowserRouter function

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,35 @@ import {
 /* pages */
 import Home from "./pages/Home/Home";
 import About from "./pages/About";
+import Academics from "./pages/Academics";
+import Rules from "./pages/Rules";
+import Photos from "./pages/Gallery/Photos";
+import Videos from "./pages/Gallery/Videos";
+import Contact from "./pages/Contact";
 
 /* layouts */
 import RootLayout from "./layouts/RootLayout";
+import GalleryLayout from "./layouts/GalleryLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="academics" element={<Academics />} />
+      <Route path="rules" element={<Rules />} />
+      <Route path="contact" element={<Contact />} />
+
+      <Route path="gallery" element={<GalleryLayout />}>
+        <Route path="photos" element={<Photos />} />
+        <Route path="videos" element={<Videos />} />
+      </Route>
 
       <Route path="*" element={<h1>Not Found</h1>} />
     </Route>
   )
 );
 
-function App() {
+export default function App() {
   return <RouterProvider router={router}></RouterProvider>;
 }
-
-export default App;
