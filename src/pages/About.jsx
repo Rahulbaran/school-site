6c368a1f7a6fb2938f28ca4b3e9ac5044126e723
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-06 17:59:54 +0530

    ci: shift About page in a directory
    - create about directory and shift About page

--- src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="about-wrapper">
-      <h1>About Section</h1>
-    </div>
-  );
-}
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="about-wrapper">
+      <header className="about-header">
+        <h1>About Us</h1>
+      </header>
+
+      <main className="container about-container">
+        <section className="about-imgs-section"></section>
+        <section className="about-content-section"></section>
+      </main>
+    </div>
+  );
+}
