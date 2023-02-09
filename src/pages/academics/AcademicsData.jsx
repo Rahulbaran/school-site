f62ef4d321ad346a041c684d2bf979b59d61f7a2
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-09 21:45:14 +0530

    ci: add required data for acadmics page in AcademicsData.jsx
    - created `AcademicsData.jsx` file inside "academics" directory
    - added and exported all required data for academics page

---
+++ src/pages/academics/AcademicsData.jsx
@@ -0,0 +1,152 @@
+export default [
+  {
+    id: 1,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/library.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/library.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/library.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/library.png 337w",
+    alt: "library",
+    title: "Library",
+    content:
+      "The Library is equipped with a large number of Educational aids, charts, Latest CDs, Play books, Multimedia Kits are a great source of Knowledge for the Students."
+  },
+  {
+    id: 2,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/extra-coaching-class.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/extra-coaching-class.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/extra-coaching-class.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/extra-coaching-class.png 337w",
+    alt: "extra coaching classes",
+    title: "Extra Coaching Classes",
+    content:
+      "We Believe in stress free education for which we have developed our system of having 6 days a week or regular classes for the students with the provision of extra classes only for those students who are found weak in particular subject."
+  },
+  {
+    id: 3,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/music-class.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/music-class.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/music-class.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/music-class.png 337w",
+    alt: "music & dance classes",
+    title: "Music & Dance Classes",
+    content:
+      "Students are taught to play musical Instruments with special emphasis on training children in Indian classical music and dance form."
+  },
+  {
+    id: 4,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/art-class.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/art-class.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/art-class.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/art-class.png 337w",
+    alt: "art room",
+    title: "Art & SUPW Room",
+    content:
+      "Children are given shape and colour to their aesthetic imaginations."
+  },
+  {
+    id: 5,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/open-air-theater.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/open-air-theater.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/open-air-theater.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/open-air-theater.png 337w",
+    alt: "open air theater",
+    title: "Open Air Theater",
+    content:
+      "A unique concept of open air theatre enables the students to present themselves in front of crowd with aplomb nad helps them in overcoming from stage fear."
+  },
+  {
+    id: 6,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/sports-facilities.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/academics-svgs/sports-facilities.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/academics-svgs/sports-facilities.png 250w, https://ik.imagekit.io/rahulbaran/school-site/academics-svgs/sports-facilities.png 337w",
+    alt: "sports facilities",
+    title: "Sports Facilities",
+    content:
+      "The school has highly equipped indoor play room and outdoor playground."
+  }
+];
+
+export const featuresLeft = [
+  {
+    id: 1,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/restroom.svg",
+    alt: "airy rooms",
+    text: "Beautiful Airy Rooms"
+  },
+  {
+    id: 2,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/school.svg",
+    alt: "coaching classes",
+    text: "Separate Coaching Classes"
+  },
+  {
+    id: 3,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/picnic.svg",
+    alt: "picnic",
+    text: "Picnic and Outings for Students"
+  },
+  {
+    id: 4,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/medical.svg",
+    alt: "medical aid",
+    text: "Medical First AID"
+  },
+  {
+    id: 5,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/drinking-water.svg",
+    alt: "drinking water",
+    text: "Pure Drinking Water"
+  },
+  {
+    id: 6,
+    src: "https://ik.imagekit.io/rahulbaran/school-site/academics-features-svgs/transport.svg",
+    alt: "transporation",
+    text: "Transport Facility"
+  },
+  {
+    id: 7,
+    src: "https://ik.imagekit.io/rahulbaran/school-site/academics-features-svgs/music.svg",
+    alt: "school band",
+    text: "School Band"
+  }
+];
+
+export const featuresRight = [
+  {
+    id: 1,
+    src: "https://ik.imagekit.io/rahulbaran/school-site/academics-features-svgs/security.svg",
+    alt: "security",
+    text: "Full Security"
+  },
+  {
+    id: 2,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/parental.svg",
+    alt: "parenting",
+    text: "Individual Parenting"
+  },
+  {
+    id: 3,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/course-content.svg",
+    alt: "",
+    text: "Latest Course content"
+  },
+  {
+    id: 4,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/analysis.svg",
+    alt: "progress analysis",
+    text: "Weekly progress analysis"
+  },
+  {
+    id: 5,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/ptm.svg",
+    alt: "regular ptm",
+    text: "Regular PTM"
+  },
+  {
+    id: 6,
+    src: "https://ik.imagekit.io/rahulbaran/school-site/academics-features-svgs/education-plan.svg",
+    alt: "education plan",
+    text: "Annual Educational Plan"
+  },
+  {
+    id: 7,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-32,h-32/school-site/academics-features-svgs/teaching-aids.svg",
+    alt: "teaching aids",
+    text: "Wooden Teaching AIDS"
+  }
+];
