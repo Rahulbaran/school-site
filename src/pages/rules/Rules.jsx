24921b03e1a2ccd8a687599e797ccd15c6eb03c3
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-14 20:03:36 +0530

    ci: update text content of h1 heading in Rules page component
    - changed text content to "Rules" in 'Rules.jsx' component

--- src/pages/rules/Rules.jsx
+++ src/pages/rules/Rules.jsx
@@ -2,8 +2,12 @@ export default function Rules() {
   return (
     <div className="rules-wrapper">
       <header className="rules-header page-header">
-        <h1>Rules & Regulations</h1>
+        <h1>Rules</h1>
       </header>
+
+      <div className="rules-container">
+        <h2>Rules & Regulations to follow</h2>
+      </div>
     </div>
   );
 }
