7563e1f73918b1c23084517ffe614a08006af9f2
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-16 07:53:46 +0530

    ci: generate cards of rules using imported data
    - used data stored in "RulesData.jsx" file to generate cards

--- src/pages/rules/Rules.jsx
+++ src/pages/rules/Rules.jsx
@@ -1,15 +1,38 @@
+import RulesData from "./RulesData";
+
 export default function Rules() {
   return (
     <div className="rules-wrapper">
       <header className="rules-header page-header">
         <h1>Rules</h1>
       </header>
-
       <div className="rules-container">
         <h2>Rules & Regulations to follow</h2>
-      </div>
 
-      <div className="rules-cards-container"></div>
+        <div className="rules-cards-container">
+          {RulesData.map(data => (
+            <div
+              className="card rules-card flex flex-col align-center justify-flex-start gap-2"
+              key={data.id}
+            >
+              <img
+                src={data.src}
+                alt={data.alt}
+                srcSet={data.srcSet}
+                loading="eager"
+                decoding="sync"
+              />
+              <h4>{data.title}</h4>
+              {data.heading && <p>{data.heading}</p>}
+              <ul>
+                {data.content.map((item, itemIndex) => (
+                  <li key={itemIndex}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
