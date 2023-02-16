c160e07a28aacb764dffe24d94d1b51828821cd1
Rahulbaran <rahulkumar109422@gmail.com>
2023-02-16 07:51:36 +0530

    ci: create "RulesData.jsx" containing cards data
    - It contains all the data of cards used in "Rules.jsx" page component

---
+++ src/pages/rules/RulesData.jsx
@@ -0,0 +1,80 @@
+export default [
+  {
+    id: 1,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/admission.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/admission.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/admission.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/admission.png 337w",
+    alt: "admission",
+    title: "Admission",
+    content: [
+      "Registration Form for all Classes can be obtained from School's Office on all working Days from the Month of February onward.",
+      "Entrance tests for admission in various classes are held in the month of February and March.",
+      "Admission to various classes is based on written test and personal interview with the principal.",
+      "School reserves the right to allow or reject an admission to a student.",
+      "If a student fails to prove standard results in the class in which he/she has been admitted, he/she may be transferred to lower class.",
+      "Photocopy of Birth Certificate issued by competent authority must accompany the registration form for classes Nursery to UKG."
+    ]
+  },
+  {
+    id: 2,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/promotion.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/promotion.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/promotion.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/promotion.png 337w",
+    alt: "evaulation and promotion",
+    title: "Evaluation & Promotion",
+    content: [
+      "There shall be three terminal examinations throughout the whole academic session.",
+      "There is a little provision to make primary students bag - less in the current session with an enforcement of continuous evaluation."
+    ]
+  },
+  {
+    id: 3,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/withdrawal.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/withdrawal.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/withdrawal.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/withdrawal.png 337w",
+    alt: "withdrawal",
+    title: "Withdrawal",
+    content: [
+      "Principal may ask a parent to remove his/her ward at any instant from the School, without explaining any reason, if it is beneficial for the school. If a parent desires for the same by conferring one month's notice before the end of the term/academic session 31st March, failing which he/she may have to deposit the fees for the next two months.",
+      "No withdrawal is possible unless all the dues are paid."
+    ]
+  },
+  {
+    id: 4,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/termination.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/termination.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/termination.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/termination.png 337w",
+    alt: "termination",
+    title: "Termination",
+    heading:
+      "School Authorities may implement ots precise to terminate a student under subsequent state of affairs:",
+    content: [
+      "If a new student fails to pay the necessary admission fee within a period of five days. If a student or parents behaves badly and uses awful words to the teachers and /or staff.",
+      "If a student is caught using unfair means at some points in Examination."
+    ]
+  },
+  {
+    id: 5,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/leave.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/leave.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/leave.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/leave.png 337w",
+    alt: "leave and attendance",
+    title: "Leave & Attendence",
+    content: [
+      "A Student must have at least 75% attendance the academic session.",
+      "The name of the student is liable to be struck off the rolls after ten days of continuous absence without permission. Repeated absence without valid reasons invite severe punishment including expulsion."
+    ]
+  },
+  {
+    id: 6,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/rules.png",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-150,h-150/school-site/rules/rules.png 150w, https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/rules/rules.png 250w, https://ik.imagekit.io/rahulbaran/school-site/rules/rules.png 337w",
+    alt: "general rules",
+    title: "General Rules",
+    content: [
+      "Students must be present at the school on a regular basis. There will not be approval of any leave of non attendance without any application.",
+      "Students will attend the school in neat and clean uniform. Students are anticipated to arrive in time at School."
+    ]
+  }
+];
