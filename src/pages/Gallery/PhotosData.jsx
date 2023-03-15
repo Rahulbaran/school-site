4a5e320b3558d8bf166171700d4785f3eb7e16be
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-15 21:31:04 +0530

    ci: include required data for "Photos" section in gallery pages

---
+++ src/pages/Gallery/PhotosData.jsx
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 0,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/khandoli",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/khandoli",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/khandoli 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/khandoli 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/khandoli 600w",
+    alt: "khandoli park"
+  },
+  {
+    id: 1,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-1",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/prayer-1",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-1 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/prayer-1 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/prayer-1 600w",
+    alt: "students praying in morning"
+  },
+  {
+    id: 2,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/sarswati-puja",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/sarswati-puja",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/sarswati-puja 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/sarswati-puja 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/sarswati-puja 600w",
+    alt: "sarswati puja celebration"
+  },
+  {
+    id: 3,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-2",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/prayer-2",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-2 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/prayer-2 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/prayer-2 600w",
+    alt: "students praying in queue"
+  },
+  {
+    id: 4,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/students",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/students",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/students 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/students 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/students 600w",
+    alt: "students of class 8"
+  },
+  {
+    id: 5,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/teachers",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/teachers 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/teachers 600w",
+    alt: "teachers get together"
+  },
+  {
+    id: 6,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-3",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/prayer-3",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-3 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/prayer-3 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/prayer-3 600w",
+    alt: "students praying"
+  },
+  {
+    id: 7,
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-4",
+    orgSrc:
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/prayer-4",
+    srcSet:
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-4 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/prayer-4 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/prayer-4 600w",
+    alt: "prayer"
+  }
+];
