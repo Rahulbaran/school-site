25d6253ab18564a71218f43c1d7c7ad2917f0401
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-15 21:32:22 +0530

    feat: create image gallery
    - imported images's data from "PhotosData"
    - mapped through it and generated images

--- src/pages/Gallery/Photos.jsx
+++ src/pages/Gallery/Photos.jsx
@@ -1,7 +1,20 @@
+import PhotosData from "./PhotosData";
+
 export default function Photos() {
   return (
     <main className="container photos-container">
-      <h2>Photos</h2>
+      <div className="photos-wrapper">
+        {PhotosData.map(data => (
+          <img
+            key={data.id}
+            src={data.src}
+            alt={data.alt}
+            srcSet={data.srcSet}
+            loading="lazy"
+            id={`image-${data.id}`}
+          />
+        ))}
+      </div>
     </main>
   );
 }
