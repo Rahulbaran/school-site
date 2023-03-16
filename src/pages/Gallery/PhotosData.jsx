a78a3eb3a5bf9873e7303540fc8e00c41085a91a
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-16 20:37:00 +0530

    ci: change urls of the photos

--- src/pages/Gallery/PhotosData.jsx
+++ src/pages/Gallery/PhotosData.jsx
@@ -37,21 +37,21 @@ export default [
   },
   {
     id: 4,
-    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/students",
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/puja-1",
     orgSrc:
-      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/students",
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/puja-1",
     srcSet:
-      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/students 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/students 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/students 600w",
-    alt: "students of class 8"
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/puja-1 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/puja-1 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/puja-1 600w",
+    alt: "sarswati puja"
   },
   {
     id: 5,
-    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers",
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers-selfie",
     orgSrc:
-      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/teachers",
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/teachers-selfie",
     srcSet:
-      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/teachers 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/teachers 600w",
-    alt: "teachers get together"
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/teachers-selfie 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/teachers-selfie 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/teachers-selfie 600w",
+    alt: "teachers selfie during sarswati puja"
   },
   {
     id: 6,
@@ -64,11 +64,11 @@ export default [
   },
   {
     id: 7,
-    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-4",
+    src: "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/new-year-drawing",
     orgSrc:
-      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/prayer-4",
+      "https://ik.imagekit.io/rahulbaran/school-site/gallery/photos/new-year-drawing",
     srcSet:
-      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/prayer-4 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/prayer-4 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/prayer-4 600w",
-    alt: "prayer"
+      "https://ik.imagekit.io/rahulbaran/tr:w-250,h-250/school-site/gallery/photos/new-year-drawing 250w, https://ik.imagekit.io/rahulbaran/tr:w-400,h-400/school-site/gallery/photos/new-year-drawing 400w, https://ik.imagekit.io/rahulbaran/tr:w-600,h-600/school-site/gallery/photos/new-year-drawing 600w",
+    alt: "children with new year drawing"
   }
 ];
