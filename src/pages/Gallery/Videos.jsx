75c9f583a1cf09f4bcae2aa757a177054c9ff8bb
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-17 16:09:11 +0530

    feat: add some contents to "Videos" section in Gallery page
    - added content about "page is under construction".

--- src/pages/Gallery/Videos.jsx
+++ src/pages/Gallery/Videos.jsx
@@ -1,7 +1,8 @@
 export default function Videos() {
   return (
     <main className="container videos-container">
-      <h2>Videos</h2>
+      <h2>404!</h2>
+      <h4>The page is under construction</h4>
     </main>
   );
 }
