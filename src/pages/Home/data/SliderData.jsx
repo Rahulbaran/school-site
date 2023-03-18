c7280a1a2d2c827bf21f4cb8f590776d8f032b9b
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-18 15:42:24 +0530

    ci: change url and other details of images used in slider in homepage
    - used school photos instead of random ones

--- src/pages/Home/data/SliderData.jsx
+++ src/pages/Home/data/SliderData.jsx
@@ -2,33 +2,33 @@ export default [
   {
     img: {
       srcset:
-        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/durgamaa.jpg 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/durgamaa.jpg 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/durgamaa.jpg 1200w",
-      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/durgamaa.jpg",
-      alt: "statue of durga maa"
+        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/teachers 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/teachers 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/teachers 1200w",
+      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/teachers",
+      alt: "all the teachers with respected persons"
     }
   },
   {
     img: {
       srcset:
-        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/girl.jpg 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/girl.jpg 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/girl.jpg 1200w",
-      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/girl.jpg",
-      alt: "a girl wearing black color one piece"
+        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-1 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/prayer-1 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/prayer-1 1200w",
+      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-1",
+      alt: "students praying in morning"
     }
   },
   {
     img: {
       srcset:
-        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/windows.jpg 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/windows.jpg 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/windows.jpg 1200w",
-      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/windows.jpg",
-      alt: "a laptop with windows 11 os"
+        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-2 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/prayer-2 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/prayer-2 1200w",
+      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-2",
+      alt: "students praying in queue"
     }
   },
   {
     img: {
       srcset:
-        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/metro.jpg 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/metro.jpg 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/metro.jpg 1200w",
-      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/metro.jpg",
-      alt: "metro in an underground platform"
+        "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-4 500w, https://ik.imagekit.io/rahulbaran/tr:w-800/school-site/slider-images/prayer-4 800w, https://ik.imagekit.io/rahulbaran/tr:w-1200/school-site/slider-images/prayer-4 1200w",
+      src: "https://ik.imagekit.io/rahulbaran/tr:w-500/school-site/slider-images/prayer-4",
+      alt: "Students getting new notices"
     }
   }
 ];
